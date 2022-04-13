ebb08181208e4d1f321e1c93eb3bb924d120bdc3
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2022-04-13 12:59:22 +0000

    Version 0.1

--- Accueil/urls.py
+++ Accueil/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name = 'index')
+    path('',views.index,name = 'index'),
+    path('connexion',views.connexion, name='connexion')
 ]
--- Accueil/views.py
+++ Accueil/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request,'accueil/index.html')
+  return render(request,'Accueil/index.html')
+
+def connexion(request):
+  return render(request,'Accueil/connexion.html')
