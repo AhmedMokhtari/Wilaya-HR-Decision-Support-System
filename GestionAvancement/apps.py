453516818973984525e872de8b4735c550bd6904
zaka_rm <zakariaemrabet1@gmail.com>
2022-06-18 00:05:53 +0100

    version 0.4

--- GestionAvancement/apps.py
+++ GestionAvancement/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class GestionavancementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GestionAvancement'
+
+    verbose_name = 'إدارة الترقيات'
--- GestionConge/apps.py
+++ GestionConge/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GestioncongeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GestionConge'
+    verbose_name = 'إدارة الرخص'
