d388b6fd2becadb6a0ca909ddf51535bbc07d7ff
zaka_rm <zakariaemrabet1@gmail.com>
2022-06-26 01:39:08 +0100

    version 0.5

--- ProjetWilaya/static/js/GestionAvancement/tboard.js
+++ ProjetWilaya/static/js/GestionAvancement/tboard.js
@@ -9,19 +9,33 @@ window.onload = function () {
                 dataType: "json",
                 success: function (response) {
                     console.log(response)
+
+                    var dataa = []
+                     for (var i = 0; i < response.length; i++) {
+                         dataa[0] = response[i].Annee.toString();
+                         dataa[1] = response[i].service;
+                         dataa[2] = response[i].pashalik;
+                         dataa[3] = response[i].cercle;
+                         dataa[4] = response[i].annexe;
+                         dataa[5] = response[i].caidat;
+                         dataa[6] = 0;
+                     }
+
                     google.charts.load('current', {'packages':['corechart']});
                       google.charts.setOnLoadCallback(drawVisualization);
+
 
                       function drawVisualization() {
                         // Some raw data (not necessarily accurate)
                         var data = google.visualization.arrayToDataTable([
                           ['الشهر', 'المصلحة', 'الباشوية', 'الدائرة', 'الملحقة', 'القيادة', '-'],
-                          ['2018',  65,      38,         22,              98,           50,      14.6],
+                          [ response[0].Annee.toString(),  response[0].service, response[0].pashalik, response[0].cercle, response[0].annexe, response[0].caidat, 0],
                           ['2019',  35,      120,        99,             268,           88,      82],
                           ['2020',  57,      167,        87,              7,           97,      23],
                           ['2021',  39,      110,        15,              68,           15,      9.4],
                           ['2022',  36,      91,         29,             26,           66,      69.6]
                         ]);
+                          console.log(dataa)
 
                         var options = {
                           title : 'عدد المترقين في الهيئات',
